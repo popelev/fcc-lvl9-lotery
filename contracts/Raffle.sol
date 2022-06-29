b7199b6e40da92390900f7f389010b2d9c187374
Ubuntu <test@test.test>
2022-06-29 09:02:45 -0700

    Init

---
+++ contracts/Raffle.sol
@@ -0,0 +1,131 @@
+//Raffle
+
+//Enter the lotery
+//Pick a random winer
+//Winner to be selected every X minutes (automaticly restart)
+//Chainlink Random Oractle
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+
+error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
+error Raffle__NotOpen();
+
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Enums */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /* State varibles */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WOEDS = 1;
+
+    /* Lotery varible */
+    address private s_recentWinner;
+    RaffleState s_raffleState;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
+
+    constructor(
+        address vrfCoordinator,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    function enterRuffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    /**
+     *  @dev This is the function that the CainLink Keeper nodes call
+     *  they look for the 'unkeeperNeeded' to return true.
+     *  The folowing should be true in order to return true:
+     *  1. Our time interval should have passed.
+     *  2. The lottery should have at least 1 playear and have some ETH.
+     *  3. Our subscription is funded with LINK.
+     *  4. The lotery should be an "open" state.
+     */
+    function checkUpkeep(
+        bytes calldata /*checkData*/
+    ) external override returns (bool upkeepNeeded, bytes memory performData) {
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+
+        bool upkeepNeeded;
+        bytes memory performData;
+
+        return (upkeepNeeded, performData);
+    }
+
+    function requestRandomWinner() external {
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATION,
+            i_callbackGasLimit,
+            NUM_WOEDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256, /*requestId*/
+        uint256[] memory randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    /* View / Pure functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-ethers")
+require("hardhat-deploy")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("solidity-coverage")
+require("dotenv").config()
+
+module.exports = {
+    solidity: "0.8.7",
+}
